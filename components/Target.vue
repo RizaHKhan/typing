1bff79262532ce82b490e025f1445cb1dbb876a6
khanr <khanriza@gmail.com>
2020-08-15 14:45:30 -0400

    Component that handles word

---
+++ components/Target.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ target }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    target: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
