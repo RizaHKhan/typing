151414ab6493c28d819f4fb52345183c8e93e890
khanr <khanriza@gmail.com>
2020-08-15 20:22:09 -0400

    Header adjustment

--- components/Header.vue
+++ components/Header.vue
@@ -2,12 +2,10 @@
   <v-card color="grey lighten-4" flat tile fixed>
     <v-toolbar dense>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-
+      <v-toolbar-title>TypingMastery</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn small class="red" to="/letters">Letters</v-btn>
-      <v-btn small class="red ml-2" to="/words">Words</v-btn>
+      <v-btn small class="red ml-2" to="/words">Typing Test</v-btn>
+      <v-btn small class="primary ml-2" to="/blog">Blog</v-btn>
     </v-toolbar>
   </v-card>
 </template>
